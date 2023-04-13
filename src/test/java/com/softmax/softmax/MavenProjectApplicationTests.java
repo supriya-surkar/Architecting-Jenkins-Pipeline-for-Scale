f1bfd0edc1e8fd70dad5fefffcc3d523c57e2406
Supriya <surkarsupriya97@gmail.com>
2023-04-13 13:57:10 -0700

    Add project skeleton

---
+++ src/test/java/com/softmax/softmax/MavenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softmax.softmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
